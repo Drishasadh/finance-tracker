2f73e61519106c59973032aae1e361f5b61ef2fc
Drisha <drishasadha@gmail.com>
2026-03-10 14:36:37 +0530

    Add finance tracker app

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+expenses = []
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        item = request.form["item"]
+        amount = request.form["amount"]
+
+        expenses.append({"item": item, "amount": amount})
+
+    return render_template("index.html", expenses=expenses)
+
+if __name__ == "__main__":
+    app.run(debug=True)
